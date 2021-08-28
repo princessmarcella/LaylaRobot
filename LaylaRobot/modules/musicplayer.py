02b0f667c0f73d8c3d9759f4ad4389d407148596
princessmarcella <79143179+princessmarcella@users.noreply.github.com>
2021-08-28 22:03:54 +0530

    Add files via upload

---
+++ LaylaRobot/modules/musicplayer.py
@@ -0,0 +1,13 @@
+__mod_name__ = "Music Player"
+
+__help__ = """
+=>> *Song Playing* 💞 
+❍ /play <song name> - play song you requested
+❍ /joinvc - To add bot in Group VC
+❍ /leavevc - To remove bot from VC
+❍ /playlist - Show now playing list
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+1) Add @princessmarcella in your group to admin with all permission
+"""
